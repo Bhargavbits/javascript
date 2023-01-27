9ec28f320defa35091a70a4f018f46a87ba04591
bhargav rana <118766633+Bhargavbits@users.noreply.github.com>
2023-01-27 21:36:24 +0530

    Create react fileuserlist.js

---
+++ react fileuserlist.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const AllUserList = () => {
+    I
+    let allUsers = [
+        {
+            name: "John",
+            email: "john@gmail.com",
+            phone: "+91-9752915531"
+          
+          
+          
+          
+        },
+        { name: "Jonny", email: "jonnyxyz@gmail.com", phone: "+91-9752915531" },
+        { name: "Alex", email: "alexxyz@gmail.com", phone: "+91-9878343210" },
+    ];
+};
+
+let array = [
+    <userDetails user={allUsers{0}} />,
+        < userDetails user = { allUsers{ 1 }} />,
+            < userDetails user = { allUsers{ 2 }} />,
+];
+return <div>{allUsers[0]}</div>;
+export default AllUserList;
